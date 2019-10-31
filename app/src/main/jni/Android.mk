0c82f83a4e0cf760935cdee0f3fb52431705ccd3
huangxiaojing <huangxiaojingcn@gmail.com>
2019-10-31 10:48:47 +0800

    Feat: 加入 bspatch 库.

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -6,7 +6,7 @@ LOCAL_C_INCLUDES := bzlib
 
 LOCAL_MODULE := bspatch
 
-LOCAL_SRC_FILES := bspatch_native.cpp bzlib/blocksort.c bzlib/huffman.c bzlib/crctable.c bzlib/randtable.c bzlib/compress.c bzlib/decompress.c bzlib/bzlib.c bzlib/bzip2.c
+LOCAL_SRC_FILES := bspatch_native.cpp bzlib/bspatch.c bzlib/blocksort.c bzlib/huffman.c bzlib/crctable.c bzlib/randtable.c bzlib/compress.c bzlib/decompress.c bzlib/bzlib.c
 
 LOCAL_LDLIBS := -llog
 
