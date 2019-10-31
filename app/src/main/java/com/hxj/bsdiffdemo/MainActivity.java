3d658fa96bd61ade624a96a1d2cf2a7b4ecce113
huangxiaojing <huangxiaojingcn@gmail.com>
2019-10-31 11:56:23 +0800

    Feat: 读取当前应用版本.

--- app/src/main/java/com/hxj/bsdiffdemo/MainActivity.java
+++ app/src/main/java/com/hxj/bsdiffdemo/MainActivity.java
@@ -1,13 +1,132 @@
 package com.hxj.bsdiffdemo;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mBtnUpdate;
+
+    private TextView mTvVersion;
+
+    static {
+        System.loadLibrary("bspatch");
+    }
+
+    public native void generateNewApkByPatch(String oldApkFile, String newApkFile, String patchFile);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtnUpdate = findViewById(R.id.btn_update);
+        mTvVersion = findViewById(R.id.tv_version);
+
+        PackageManager packageManager = getPackageManager();
+        try {
+            PackageInfo packageInfo = packageManager.getPackageInfo(getPackageName(), 0);
+            String versionName = packageInfo.versionName;
+            Log.i("James", "onCreate versionName: " + versionName);
+            mTvVersion.setText("当前的版本：" + versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        mBtnUpdate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                            Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                        // 自定义提示框.
+                    } else {
+                        ActivityCompat.requestPermissions(MainActivity.this,
+                                new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
+                                100);
+                    }
+                } else {
+                    update();
+                }
+            }
+        });
+    }
+
+    private void update() {
+
+        new UpdateApkAsyckTask().execute();
+    }
+
+    class UpdateApkAsyckTask extends AsyncTask<Void, Void, File> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected File doInBackground(Void... voids) {
+            // 获取当前 apk 安装的路径.
+            String oldApkFilePath = getApplicationInfo().sourceDir;
+
+            // 新 apk 所在目录.
+            File newApkFile = new File(Environment.getExternalStorageDirectory(), "test_new.apk");
+            boolean newApkFileExist = false;
+            if (!newApkFile.exists()) {
+                try {
+                    newApkFileExist = newApkFile.createNewFile();
+                } catch (IOException e) {
+                    newApkFileExist = false;
+                    e.printStackTrace();
+                }
+            } else {
+                newApkFileExist = true;
+            }
+
+            if (!newApkFileExist) {
+                Toast.makeText(MainActivity.this, "创建文件失败", Toast.LENGTH_SHORT).show();
+                return null;
+            }
+
+            // 服务器下载后的补丁文件
+            File patchFile = new File(Environment.getExternalStorageDirectory(), "test.patch");
+            if (!patchFile.exists()) {
+                Toast.makeText(MainActivity.this, "暂未发现新版本", Toast.LENGTH_SHORT).show();
+            }
+
+            // 获取新的 apk 安装路径.
+            String newApkFilePath = newApkFile.getAbsolutePath();
+
+            // 获取补丁文件路径
+            String patchFilePath = patchFile.getAbsolutePath();
+
+            generateNewApkByPatch(oldApkFilePath, newApkFilePath, patchFilePath);
+
+            return newApkFile;
+        }
+
+        @Override
+        protected void onPostExecute(File file) {
+            super.onPostExecute(file);
+
+            Log.i("James", "onPostExecute: " + file.getTotalSpace());
+        }
     }
 }
--- app/src/main/jni/bspatch_native.cpp
+++ app/src/main/jni/bspatch_native.cpp
@@ -1,1 +1,30 @@
+#include <jni.h>
 
+extern int main(int argc,char * argv[]);
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_hxj_bsdiffdemo_MainActivity_generateNewApkByPatch(JNIEnv *env, jobject jobj,
+        jstring old_apk_file_, jstring new_apk_file_, jstring patch_file_);
+
+
+
+void Java_com_hxj_bsdiffdemo_MainActivity_generateNewApkByPatch(JNIEnv *env, jobject jobj,
+        jstring old_apk_file_, jstring new_apk_file_, jstring patch_file_) {
+    const char *old_apk_file = env->GetStringUTFChars(old_apk_file_, NULL);
+    const char *new_apk_file = env->GetStringUTFChars(new_apk_file_, NULL);
+    const char *patch_file = env->GetStringUTFChars(patch_file_, NULL);
+
+    char *args[4];
+
+    args[0] = (char *)"bspatch";
+    args[1] = (char *) old_apk_file;
+    args[2] = (char *) new_apk_file;
+    args[3] = (char *) patch_file;
+
+    main(4, args);
+
+    env->ReleaseStringUTFChars(old_apk_file_, old_apk_file);
+    env->ReleaseStringUTFChars(new_apk_file_, new_apk_file);
+    env->ReleaseStringUTFChars(patch_file_, patch_file);
+}
